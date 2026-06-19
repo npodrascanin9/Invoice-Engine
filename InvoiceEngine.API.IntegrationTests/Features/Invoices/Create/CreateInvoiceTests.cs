05ab09d13ee7118da1545945ed26069847370aa9
Nikola Podrascanin <nikola.podrascanin@eng.it>
2026-06-19 20:05:17 +0200

    CreateInvoice integration test v1

--- InvoiceEngine.API.IntegrationTests/Features/Invoices/Create/CreateInvoiceTests.cs
+++ InvoiceEngine.API.IntegrationTests/Features/Invoices/Create/CreateInvoiceTests.cs
@@ -5,6 +5,25 @@ namespace InvoiceEngine.API.IntegrationTests.Features.Invoices.Create;
 public class CreateInvoiceTests :
     BaseIntegrationTest
 {
+    private readonly Faker<CreateInvoiceCommand> _commandGenerator 
+        = new Faker<CreateInvoiceCommand>()
+            .CustomInstantiator(factoryMethod => new CreateInvoiceCommand(
+                Incoterm: factoryMethod.PickRandom<IncotermRule>(),
+                IssuedAt: DateOnly.FromDateTime(factoryMethod.Date.Recent(5)),
+                ExpiresAt: DateOnly.FromDateTime(factoryMethod.Date.Soon(30)),
+                TransactionStartDate: DateOnly.FromDateTime(factoryMethod.Date.Recent(10)),
+                TransactionEndDate: DateOnly.FromDateTime(factoryMethod.Date.Soon(20)),
+                ClientSellerId: 1,
+                ClientBuyerId: 2,
+                Items: new Dictionary<InvoiceItemTypeCode, CreateInvoiceItemRequest>
+                {
+                    { InvoiceItemTypeCode.SellGoods, new CreateInvoiceItemRequest(factoryMethod.Commerce.ProductName(), factoryMethod.Random.Decimal(100, 2000)) },
+                    { InvoiceItemTypeCode.Transport, new CreateInvoiceItemRequest("Transport service", factoryMethod.Random.Decimal(50, 500)) },
+                    { InvoiceItemTypeCode.Insurance, new CreateInvoiceItemRequest("Insurance policy", factoryMethod.Random.Decimal(20, 300)) }
+                },
+                CustomIncotermRules: null
+            ));
+
     public CreateInvoiceTests(
         IntegrationTestWebAppFactory factory)
         : base(factory)
@@ -16,7 +35,7 @@ public class CreateInvoiceTests :
     public async Task ShouldCreateInvoice()
     {
         // Arrange
-        var command = new CreateInvoiceCommand();
+        var command = _commandGenerator.Generate();
 
         // Act
         var result = await Sender.Send(command);
